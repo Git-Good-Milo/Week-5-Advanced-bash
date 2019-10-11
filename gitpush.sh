6d4b2bdd7f8cb1ee38e18f23424f65e660add296
Miles Eastwood <MEastwood@spartaglobal.com>
2019-10-11 15:04:01 +0100

    lol

--- gitpush.sh
+++ gitpush.sh
@@ -6,7 +6,7 @@ read commitMessage
 
 git commit -m "$commitMessage"
 
-git remote set-url origin git@https://github.com/Git-Good-Milo/Week-5-Advanced-bash
+git remote set-url origin https://github.com/Git-Good-Milo/Week-5-Advanced-bash
 # echo 'Enter the name of the branch:'
 # read branch
 
