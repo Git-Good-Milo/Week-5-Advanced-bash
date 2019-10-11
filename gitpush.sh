cd8374ffdf37a68f7cbdd024bdbefb71ab718d12
Miles Eastwood <MEastwood@spartaglobal.com>
2019-10-11 14:57:58 +0100

    lol

--- gitpush.sh
+++ gitpush.sh
@@ -1,10 +1,12 @@
 git add .
 
+# git clone https://github.com/jakubroztocil/cloudtunes.giy
 echo 'Enter the commit message:'
 read commitMessage
 
 git commit -m "$commitMessage"
 
+git remote set-url origin git@https://github.com/Git-Good-Milo/Week-5-Advanced-bash
 # echo 'Enter the name of the branch:'
 # read branch
 
